a7c1c2b7401ee7d4749465c4e96177ea72f7a5bd
Samuel Olagunju <dioxair@gmail.com>
2022-06-03 13:32:10 -0400

    Add request variable with user agent and url

--- saveeit.py
+++ saveeit.py
@@ -5,3 +5,5 @@ def main():
     print("Please enter the URL of the Reddit video you want to download.")
 
     redditURL = input(">> ")[:-1]+".json"
+
+    request = requests.get(redditURL, headers = { "User-agent": "Mozilla/5.0 (X11; Linux x86_64; rv:101.0) Gecko/20100101 Firefox/101.0" })
